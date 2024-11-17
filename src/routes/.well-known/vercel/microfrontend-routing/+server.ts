09382612aec3eb571977878f8cbb9b9d5f0775ac
adomakins <adamskjervold@gmail.com>
2024-11-16 21:22:25 -0800

    Fixed build issue

---
+++ src/routes/.well-known/vercel/microfrontend-routing/+server.ts
@@ -0,0 +1,4 @@
+// src/routes/.well-known/vercel/microfrontend-routing/+server.js
+export async function GET() {
+	return new Response('Microfrontend routing is not implemented', { status: 200 });
+}
